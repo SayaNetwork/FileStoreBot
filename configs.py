3a6e230fcae38775e1f24054565b67bfa750c3c9
SHNWAZ <271950542+shnwazdeveloper@users.noreply.github.com>
2026-07-06 19:32:07 +0530

    Fix configuration bugs: handle missing env vars, correct BOT_OWNER key, fix boolean parsing

--- configs.py
+++ configs.py
@@ -1,22 +1,22 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
+  API_ID = int(os.environ.get("API_ID", "0"))
   API_HASH = os.environ.get("API_HASH", "")
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "0"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "")
-  SHORTLINK_API = os.environ.get('pk_exCiUGCVgdbUYgXs', "")
-  BOT_OWNER = int(os.environ.get("kidzmc", ""))
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "0"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-  BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
-  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-  BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
-  BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
-  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+  BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split() if x.isdigit())
+  FORWARD_AS_COPY = os.environ.get("FORWARD_AS_COPY", "True").lower() in ("true", "1", "yes")
+  BROADCAST_AS_COPY = os.environ.get("BROADCAST_AS_COPY", "True").lower() in ("true", "1", "yes")
+  BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split() if x.isdigit()))
+  OTHER_USERS_CAN_SAVE_FILE = os.environ.get("OTHER_USERS_CAN_SAVE_FILE", "True").lower() in ("true", "1", "yes")
   ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
 Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
@@ -45,5 +45,5 @@ How to Use Bot & it's Benefits??
 
 📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
 
-⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
+⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe fr[...]
 """
