932e27ccc7b66005e3bdf85d26e2686c3d1a2342
SHNWAZ <271950542+shnwazdeveloper@users.noreply.github.com>
2026-07-06 19:07:46 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -34,7 +34,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/VJ_Bot_Disscussion).",
+                text="Sorry Sir, You are Banned to use me. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/SayaProject).",
                 disable_web_page_preview=True
             )
             return 400
@@ -63,7 +63,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/VJ_Bot_Disscussion).",
+            text="Something went Wrong. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/SayaProject).",
             disable_web_page_preview=True
         )
         return 200
